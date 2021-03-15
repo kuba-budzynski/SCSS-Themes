05143c883bd7041384473250b60c32bb8dc85a09
Anna <annapapros21@gmail.com>
2021-03-15 10:20:18 +0100

    add some informations, modal and breakpoints

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../css/SolarSystem.scss'
 import { planets } from '../constant/planets'
 
-const SolarSystem = () => {
+const Modal = ({planet, onClose}) => {
     return (
+        <div className="backdrop" onClick={onClose}>   
+            <section className="modal" onClick={e => e.stopPropagation()}>                        
+                <h2>{planet.name}</h2>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+                <img src={planet.src} alt={planet.name}/>
+                <button className="closeButton" onClick={onClose}>Zamknij</button> 
+            </section>
+        </div>
+    );
+}
+const SolarSystem = () => {
+    const [selectedPlanet, setSelectedPlanet] = useState(null);
+
+    return (
+        <>
         <section className="planets">
             <h2>Układ słoneczny</h2>
-            <ul>
+            <ul className="planets-list">
                 {planets.map((planet) => 
-                    <li key={planet.name}>
-                        <img src={planet.src} alt={planet.name}/>
-                        <span>{planet.name}</span>
+                    <li key={planet.name} onClick={() => setSelectedPlanet(planet)}>
+                        <div><img src={planet.src} alt={planet.name}/></div>
+                        <p>{planet.name}</p>
                     </li>
                 )}
             </ul>
         </section>
+        {selectedPlanet && <Modal planet={selectedPlanet} onClose={() => setSelectedPlanet(null)}/>}
+        </>
     );
 }
 
--- src/constant/planets.js
+++ src/constant/planets.js
@@ -4,30 +4,48 @@ import Earth from '../images/earth.png'
 import Mars from '../images/mars.png'
 import Jupiter from '../images/jupiter.png'
 import Saturn from '../images/saturn.png'
+import Uran from '../images/uran.png'
+import Neptun from '../images/neptune.png'
 
 export const planets = [
     {
         name: 'Merkury',
-        src: Merkury
+        src: Merkury,
+        size: 4879,
     },
     {
         name: 'Wenus',
-        src: Venus
+        src: Venus,
+        size: 12104,
     },
     {
         name: 'Ziemia',
-        src: Earth
+        src: Earth,
+        size: 12756,
     },
     {
         name: 'Mars',
-        src: Mars
+        src: Mars,
+        size: 6805,
     },
     {
         name: 'Jowisz',
-        src: Jupiter
+        src: Jupiter,
+        size: 142984,
     },
     {
         name: 'Saturn',
-        src: Saturn
+        src: Saturn,
+        size: 120536,
+    },
+    {
+        name: 'Uran',
+        src: Uran,
+        size: 51118,
+    },
+    {
+        name: 'Neptun',
+        src: Neptun,
+        size: 49528,
     },
 ]
